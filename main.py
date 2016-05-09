e24f7407dd8682ddc92bafeb1eb67126e69187e6
eldilibra <writeme@ldlibra.com>
2016-05-08 23:52:41 -0400

    fixed gitignore, fixed main.py typo

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import falcon
-import pyscopg2
+import psycopg2
 
 conn = psycopg2.connect("dbname=roommate_app user=postgres")
 
 cur = conn.cursor()
 
 class RoommateResource:
-    def on_get(self, req, resp):
-    	cur.execute("SELECT * FROM roommates;")
-			roommates = cur.fetchall()
-			resp.status = falcon.HTTP_200
-			resp.body = roommates
+	def on_get(self, req, resp):
+		cur.execute("SELECT * FROM roommates;")
+		roommates = cur.fetchall()
+		resp.status = falcon.HTTP_200
+		resp.body = roommates
 
 app = falcon.API()
 
